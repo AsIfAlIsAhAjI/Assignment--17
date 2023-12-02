6f4089503b56d6d29bd4621c6cfc2ec4e6d63abe
AsIfAlIsAhAjI <152707064+AsIfAlIsAhAjI@users.noreply.github.com>
2023-12-02 22:06:51 +0530

    Create qn-3.c

---
+++ qn-3.c
@@ -0,0 +1,21 @@
+//3. Write a program to count vowels in a given string
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[100],a[6]="aeiou";
+    int i,j,count=0;
+    printf("enter a word:\n");
+    fgets(str,100,stdin);
+
+    for(i=0;a[i];i++)
+    {
+        for(j=0;str[j];j++)
+        {
+            if(a[i]==str[j])
+                count++;
+        }
+    }
+    printf("%d vowels in this string ",count);
+}
