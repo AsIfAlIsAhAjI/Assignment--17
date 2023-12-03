00a15e024b468998321d200d5f8b4a1fa126d843
AsIfAlIsAhAjI <152707064+AsIfAlIsAhAjI@users.noreply.github.com>
2023-12-03 13:58:34 +0530

    Update qn-4.c

--- qn-4.c
+++ qn-4.c
@@ -19,3 +19,21 @@ int main()
 
     printf("%s",str);
 }
+//another process:
+
+/*#include<stdio.h>
+int main()
+{
+    char str[100];
+    int i;
+    printf("enter a word:\n");
+    fgets(str,100,stdin);
+
+    for(i=0;str[i];i++)
+    {
+        if(str[i]>='a'&& str[i]<='z')
+            str[i]-=32;
+    }
+    printf("%s",str);
+}*/
+
